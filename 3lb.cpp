cc03f8f77ec85efca157a9c5047c22d432d2f151
FOOZBY <alexeysharonin15@gmail.com>
2022-06-29 15:26:48 +0300

    Добавьте файлы проекта.

---
+++ 3lb.cpp
@@ -0,0 +1,88 @@
+﻿#include <SFML/Graphics.hpp>
+using namespace sf;
+RenderWindow* window;
+Mutex mutex;
+
+CircleShape* triangle;
+//RectangleShape* rectangle;
+CircleShape* circle;
+
+void render()
+{
+    while (window->isOpen())
+    {
+        sleep(milliseconds(1));
+
+        mutex.lock();
+        window->setActive(true);
+
+        window->clear(Color::White);
+        window->draw(*circle);
+        window->draw(*triangle);
+        window->display();
+
+        Event event;
+        while (window->pollEvent(event)) {
+            if (event.type == Event::Closed) {
+                window->close();
+            }
+        }
+
+        window->setActive(false);
+        mutex.unlock();
+    }
+}
+
+
+void triangleUpdate()
+{
+    for (int i = 0; i < 55; i++) {
+        mutex.lock();
+        window->setActive(true);
+        triangle->move(10, 0);
+        window->setActive(false);
+        mutex.unlock();
+        sleep(milliseconds(50));
+    }
+}
+
+void circleUpdate()
+{
+    for (int i = 0; i < 35; i++) {
+        mutex.lock();
+        window->setActive(true);
+        circle->move(-10, 0);
+        window->setActive(false);
+        mutex.unlock();
+        sleep(milliseconds(100));
+    }
+}
+
+int main()
+{
+    window = new RenderWindow(VideoMode(1080, 720), "Lab3");
+    window->setFramerateLimit(60);
+
+    triangle = new CircleShape(75, 3);
+    triangle->setPosition(100, 100);
+    triangle->setFillColor(Color::Black);
+
+    circle = new CircleShape();
+    circle->setRadius(75);
+    circle->setPosition(window->getSize().x*0.6, 300);
+    circle->setFillColor(Color::Black);
+
+    window->clear(Color::White);
+    window->draw(*circle);
+    window->draw(*triangle);
+    window->setActive(false);
+
+    Thread triangleUpdateThread(&triangleUpdate);
+    Thread circleUpdateThread(&circleUpdate);
+
+    triangleUpdateThread.launch();
+    circleUpdateThread.launch();
+
+    render();
+}
+
